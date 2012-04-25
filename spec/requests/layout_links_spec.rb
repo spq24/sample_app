be934087d66b625397e52b193906690fb326ba77
Steve Quatrani <spq2461@gmail.com>
2012-04-25 00:01:38 -0400

    Added a Users controller and layout links test

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe "LayoutLinks" do
- it	"should have at '/'" do
+ it	"should have a Home Page at '/'" do
  	get '/'
  	response.should have_selector('title', :content => "Home")
   end
@@ -11,14 +11,32 @@ describe "LayoutLinks" do
 	response.should have_selector('title', :content => "Contact")
   end
 
-    it "Should have a Contact page at '/about'" do
+    it "Should have a About page at '/about'" do
 	get '/about'
 	response.should have_selector('title', :content => "About")
   end
 
-  it "Should have a Contact page at '/help'" do
+  it "Should have a Help page at '/help'" do
 	get '/help'
 	response.should have_selector('title', :content => "Help")
+  end
+
+  it "should have a signup page at '/signup'" do
+    get '/signup'
+    response.should have_selector('title', :content => "Sign Up")
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    response.should have_selector('title', :content => "Home")
+    click_link "About"
+    response.should have_selector('title', :content => "About")
+    click_link "Contact"
+    response.should have_selector('title', :content => "Contact")
+    response.should have_selector('title', :content => "Home")
+    click_link "Sign up now!"
+    response.should have_selector('title', :content => "Sign Up")
+    response.should have_selector('a[href="/"]>img')
   end
 
 end
