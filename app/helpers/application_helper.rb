b1ba21fd0b9888a7cca1659c900b705fd5bb6c59
Steve Quatrani <spq2461@gmail.com>
2012-04-24 23:36:29 -0400

    added routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,7 +7,10 @@ module ApplicationHelper
 			base_title
 		else
 			"#{base_title} | #{@title}"
-
 		end
+	end
+
+	def logo
+		image_tag("logo.png", :alt => "Sample App", :class => "round")	
 	end
 end
