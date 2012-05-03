f90e70373df89fa66983ea9ff507fb18a9c7dcbe
Steve Quatrani <spq2461@gmail.com>
2012-05-03 18:22:08 -0400

    Added a sessions controller and signin view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,5 +10,14 @@ class UsersController < ApplicationController
   	@title = "Sign up"
   end
 
-
+  def create
+  	@user = User.new(params[:user])
+  	if @user.save
+  		flash[:success] = "Welcome to the Sample App!"
+  		redirect_to @user
+  	else	
+  		@title = "Sign Up"	
+  		render 'new'
+  		end
+	end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -41,8 +41,6 @@ end
 
   describe "GET 'new'" do
 
-    
-
     it "returns http success" do
       get :new
       response.should be_success
@@ -51,6 +49,51 @@ end
   it "should have the right title" do
   	get :new
   	response.should have_selector('title', :content => "Sign up")
+    end
+  end
+
+  describe "POST 'create'" do
+
+    describe "failure" do
+
+      before(:each) do
+        @attr = { :name => "", :email => "", :password => "", :password_confirmation => "" }
+      end
+
+      it "should have the right title" do
+        post :create, :user => @attr
+        response.should have_selector('title', :content => "Sign Up")
+      end
+
+      it "should render a new page" do
+        post :create, :user => @attr
+        response.should render_template('new')
+      end
+
+      it "should not create a user" do
+        lambda do 
+          post :create, :user => @attr
+          
+        end.should_not change(User, :count)
+      end
+    end
+    describe "success" do
+
+      before(:each) do
+        @attr = { :name => "New User", :email => "user@example.com", :password => "foobar", :password_confirmation => "foobar" }
+      end
+
+      it "should create a user" do
+         lambda do 
+         post :create, :user => @attr 
+        end.should change(User, :count).by(1)
+      end
+
+      it "should redirect to  the user show page" do 
+        post :create, :user => @attr
+        response.should redirect_to(user_path(assigns(:user)))
+      end
+      
     end
   end
 end
