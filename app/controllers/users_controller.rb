9977d65e871f00a064d1ee6ac47a11a1a2b9fa45
Steve Quatrani <spq2461@gmail.com>
2012-05-04 20:47:36 -0400

    Editing and updating users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,10 +15,26 @@ class UsersController < ApplicationController
   	if @user.save
       sign_in @user
   		flash[:success] = "Welcome to the Sample App!"
-  		redirect_to @user
+  		redirect_to @user, :flash => { :success => "Welcome to the Sample App!" }
   	else	
   		@title = "Sign Up"	
   		render 'new'
   		end
 	end
+
+  def edit 
+    @user = User.find(params[:id])
+    @title = "Edit user"
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      redirect_to @user, :flash => { :success => "Profile updated." }
+    else
+    @title = "Edit user"
+    render 'edit'
+  end
 end
+end
+
