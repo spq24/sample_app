87ecb97447ed240fedffb377dcd5e8b0af23a9bf
Steve Quatrani <spq2461@gmail.com>
2012-05-06 18:12:11 -0400

    friendly forwarding and correct user requirement

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   before_filter :authenticate, :only => [:edit, :update]
- 
+  before_filter :correct_user, :only => [:edit, :update]
    def show
   	@user = User.find(params[:id])
   	@title = @user.name
@@ -42,12 +42,14 @@ end
 private
 
   def authenticate
-    flash[:notice] = "Please sign in to access this page."
     deny_access unless signed_in?
   end
 
-  def deny_access
-    redirect_to signin_path, :notice => "Please sign in to access this page."
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless @user == current_user
   end
+
+
 end
 
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -209,6 +209,8 @@ end
       @user = Factory(:user)
     end
 
+    describe "for non-signed-in users" do
+   
     it "should deny access to 'edit'" do
       get :edit,  :id => @user
       response.should redirect_to(signin_path)
@@ -219,5 +221,25 @@ end
       put :update, :id => @user, :user => {}
        response.should redirect_to(signin_path)
      end
+    end
+
+    describe "for signed-in users" do
+
+      before(:each) do
+        wrong_user = Factory(:user, :email => "user@example.net")
+        test_sign_in(wrong_user)
+      end
+
+      it "should require matching users for 'edit'"
+        get :edit, :id => @user
+        response.should redirect_to(root_path)
+      end
+
+      it "should require matching users for 'update'"
+        put :update, :id => @user, :user => {}
+        response.should redirect_to(root_path)
+      end
+    end
+
   end
 end
