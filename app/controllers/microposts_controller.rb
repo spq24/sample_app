5ce7b381326349338233ec0f24ae21292e9c3dfc
Steve Quatrani <spq2461@gmail.com>
2012-05-13 01:48:52 -0400

    added microposts and destory methods

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
-	before_filter :authenticate
+	  before_filter :authenticate, :only => [:create, :destroy]
+  	  before_filter :authorized_user, :only => :destroy
 
 	
 	def create
@@ -7,11 +8,20 @@ class MicropostsController < ApplicationController
 		if @micropost.save
 			redirect_to root_path, :flash => { :success => "Micropost Created" }
 		else
+		@feed_items = []
 		render 'pages/home'
 		end
 	end
 
 	def destroy
+		@micropost.destroy
+		redirect_back_or root_path
 	end
 
+  private
+
+    def authorized_user
+      @micropost = current_user.microposts.find_by_id(params[:id])
+      redirect_to root_path if @micropost.nil?
+    end
 end
